110e7159590fdea63cd6a754879153230acd5409
Dylano <dylano.hartman@gmail.com>
2020-03-26 11:25:49 +0100

    activity styling and element setup

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -8,13 +8,49 @@ const ActivityContainer = styled.div`
   align-items: center;
 `
 
-const ActivityItem = styled.div`
+const ActivityItem = styled.div<StyleProps>`
   width: 80%;
-  height: 40px;
+  height: ${({ toggle }) => (toggle ? '200px' : '40px')};
+  transition: 0.2s;
   box-shadow: ${colors.shadows.default};
   display: flex;
-  align-items: center;
+  align-items: flex-start;
+  flex-direction: column;
 `
+
+const ActivityBar = styled.div`
+  width: 100%;
+`
+
+const Details = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: unset;
+`
+const Line = styled.hr`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  border-width: 1px;
+  width: 70%;
+`
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+`
+
+const Detail = styled.li`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+`
+
+interface StyleProps {
+  toggle: boolean
+}
 
 interface Props {
   name: string
@@ -35,16 +71,46 @@ interface Props {
 }
 
 const Activity = (props: Props) => {
+  const [toggle, setToggle] = React.useState(false)
+
   {
-    console.log(props.when)
+    console.log(props)
+    console.log(toggle)
   }
   return (
     <ActivityContainer>
-      <span>11</span>
-      <ActivityItem>
-        <p>{props.categoryName}</p>
-        <p>{props.name}</p>
-        <button>v</button>
+      <span></span>
+      <ActivityItem toggle={toggle}>
+        <ActivityBar>
+          <span>{props.categoryName}</span>
+          <span>{props.name}</span>
+          <button onClick={(): void => setToggle(!toggle)}>v</button>
+        </ActivityBar>
+
+        {toggle && (
+          <>
+            <Details>
+              <Detail>
+                <span>i</span> {props.room}
+              </Detail>
+              <Detail>
+                <span>i</span> {props.room}
+              </Detail>
+              <Detail>
+                <span>i</span> {props.room}
+              </Detail>
+              <Detail>
+                <span>i</span> {props.room}
+              </Detail>
+            </Details>
+            <Line />
+            <Buttons>
+              <button>Ja</button>
+              <button>Misschien</button>
+              <button>Nee</button>
+            </Buttons>
+          </>
+        )}
       </ActivityItem>
     </ActivityContainer>
   )
