4ff4ec1b724bd8855f497297e1c0132ce55ca971
Max Altena <Max@MaxAltena.com>
2020-03-26 12:12:26 +0100

    added beforeinstallprompt

--- src/setupEvents.ts
+++ src/setupEvents.ts
@@ -21,8 +21,42 @@ export const onlineStatus = (): void => {
   window.addEventListener('offline', updateOnlineStatus)
 }
 
+export const addBeforeInstallPrompt = (): void => {
+  let deferredPrompt: BeforeInstallPromptEvent
+
+  window.addEventListener(
+    'beforeinstallprompt',
+    (e: BeforeInstallPromptEvent) => {
+      console.dir(e)
+
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault()
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e
+      // Update UI notify the user they can install the PWA
+      // showInstallPromotion()
+      alert('cancelled')
+      setTimeout(() => {
+        deferredPrompt.prompt()
+        alert('prompted')
+
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt')
+            alert('accepted')
+          } else {
+            console.log('User dismissed the install prompt')
+            alert('dismissed')
+          }
+        })
+      }, 5000)
+    },
+  )
+}
+
 export const init = (): void => {
   window.addEventListener('load', () => {
     onlineStatus()
+    addBeforeInstallPrompt()
   })
 }
