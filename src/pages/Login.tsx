37b2cc22d33a6af0bbe346e786328cd642819ed2
timo <timo.van.de.laar.tvdl@gmail.com>
2020-04-02 17:01:39 +0200

    Add image and tekst to login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import 'firebaseui/dist/firebaseui.css'
 import { fireUI } from 'src/utils/firebase'
 import configFirebaseUI from 'src/config/firebaseUI'
 import { Header } from 'src/components'
+import Logo from 'src/assets/general/logo-stichting-Ik-Wil.svg'
 
 const Login: React.FC = () => {
   React.useEffect(() => {
@@ -11,7 +12,9 @@ const Login: React.FC = () => {
 
   return (
     <>
-      <Header title="Inloggen" />
+      <Header title="Inloggen"/>
+      <div style={{height: "auto", width: "60vw", margin: "auto", paddingTop: "3vh"}}><img src={Logo}/></div>
+      <h3 style={{textAlign: "center", padding: "3vh"}}>Welkom bij Stichting ik wil</h3>
       <div id="firebase-auth-container"></div>
     </>
   )
