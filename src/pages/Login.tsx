4ede70d2030f2f172545941bff384bc638ca6c2c
Max Altena <Max@MaxAltena.com>
2020-04-07 16:48:23 +0200

    Added notifications to the app

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,59 @@
 import * as React from 'react'
 import 'firebaseui/dist/firebaseui.css'
-import { fireUI } from 'src/utils/firebase'
+import { fireUI, fireStore, fireMessaging } from 'src/utils/firebase'
 import configFirebaseUI from 'src/config/firebaseUI'
 import { Header } from 'src/components'
+import { connect } from 'react-redux'
+import { RootState } from 'src/redux/store'
+import { RouteComponentProps } from 'react-router'
 
-const Login: React.FC = () => {
+interface OwnProps {}
+
+interface StateProps {
+  isLoggedIn: boolean
+}
+
+type Props = OwnProps & StateProps & RouteComponentProps
+
+const Login: React.FC<Props> = (props: Props) => {
+  const askForPermission = async (uid: string): Promise<any> => {
+    await fireMessaging
+      .requestPermission()
+      .then(async () => {
+        const token = await fireMessaging.getToken()
+        fireStore.collection('users').doc(uid).update({ pushToken: token })
+      })
+      .catch((err) => console.error(err)) // eslint-disable-line no-console
+  }
+
   React.useEffect(() => {
-    fireUI.start('#firebase-auth-container', configFirebaseUI)
-  }, [])
+    if (props.isLoggedIn) props.history.push('/')
+
+    fireUI.start('#firebase-auth-container', {
+      ...configFirebaseUI,
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult): boolean => {
+          askForPermission(authResult.user.uid)
+          return false
+        },
+      },
+    })
+  }, [props.isLoggedIn, props.history])
 
   return (
     <>
       <Header title="Inloggen" />
+
       <div id="firebase-auth-container"></div>
     </>
   )
 }
 
-export default Login
+const mapStateToProps = (state: RootState, ownProps: OwnProps): StateProps => {
+  return {
+    ...ownProps,
+    isLoggedIn: !state.firebase.auth.isEmpty,
+  }
+}
+
+export default connect(mapStateToProps, null)(Login)
