44a3e6c525449309067cbc25a895f2e0925a6453
timo <timo.van.de.laar.tvdl@gmail.com>
2020-04-07 15:35:22 +0200

    fix pull request #93
    Asset filename changed to lowercases.
    No alt tag added to the img tag.
    darkgrey added to h3
    use of styled components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,24 @@ import 'firebaseui/dist/firebaseui.css'
 import { fireUI } from 'src/utils/firebase'
 import configFirebaseUI from 'src/config/firebaseUI'
 import { Header } from 'src/components'
-import Logo from 'src/assets/general/logo-stichting-Ik-Wil.svg'
+import Logo from 'src/assets/general/logo-stichting-ik-wil.svg'
+import { colors } from 'src/styles/colors'
+import styled from 'styled-components'
+
+
+const StyledLogo = styled.div`
+  height: auto;
+  width: 60vw;
+  max-width: 220px;
+  margin: auto;
+  padding-top: 3vh;
+`
+const StyledTitle = styled.h3`
+  text-align: center;
+  padding: 3vh;
+  color : ${colors.darkgrey};
+`
+
 
 const Login: React.FC = () => {
   React.useEffect(() => {
@@ -13,20 +30,13 @@ const Login: React.FC = () => {
   return (
     <>
       <Header title="Inloggen" />
-      <div
-        style={{
-          height: 'auto',
-          width: '60vw',
-          maxWidth: '220px',
-          margin: 'auto',
-          paddingTop: '3vh',
-        }}
-      >
-        <img src={Logo} />
-      </div>
-      <h3 style={{ textAlign: 'center', padding: '3vh' }}>
+      <StyledLogo>
+        <img src={Logo} alt="Ik_wil_logo"/>
+      </StyledLogo>
+     
+      <StyledTitle>
         Welkom bij Stichting ik wil
-      </h3>
+      </StyledTitle>
       <div id="firebase-auth-container"></div>
     </>
   )
